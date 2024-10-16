737db8f846fa33058b039419e83fdc8073ce3c0e
wateenalreshoodi <wateenalrshoudi@gmail.com>
2024-10-16 21:18:13 +0300

    Resolved merge conflicts

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.bookmodule',
+    'apps.usermodule',
 ]
 
 MIDDLEWARE = [
