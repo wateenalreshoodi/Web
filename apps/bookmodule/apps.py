5dd58f6bc89c973e717726b659489f22efc665e9
wateenalreshoodi <wateenalrshoudi@gmail.com>
2024-10-16 20:53:10 +0300

    Updated project files

--- apps/bookmodule/apps.py
+++ apps/bookmodule/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class BookmoduleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookmodule'
+    name = 'apps.usermodule'
+
