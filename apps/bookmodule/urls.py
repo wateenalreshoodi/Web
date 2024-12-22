2f39e50137b0d48fdda33bf865771eb4992ec5f3
wateenalreshoodi <wateenalrshoudi@gmail.com>
2024-12-22 03:47:36 +0300

    lab10

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -28,11 +28,11 @@ urlpatterns = [
     path('html5/listing/', views.listing, name="books.listing"),
     path('html5/tables/', views.tables, name="books.tables"),
 
-    # Query Views
-    path('simple/query/', views.simple_query, name="books.simple_query"),
-    path('complex/query/', views.complex_query, name="books.complex_query"),
-    path('lookup/query/', views.lookup_query, name="books.lookup_query"),
-    path('search/', views.search_books, name="books.search"),
+    # Query Views# Query Views
+path('simple/query/', views.simple_query, name="books.simple_query"),
+path('complex/query/', views.complex_query, name="books.complex_query"),
+path('lookup/query/', views.lookup_query, name="books.lookup_query"),
+path('search/', views.search_books, name="books.search"),
 
     # Student Views
     path('students/', views.list_students, name='students.list'),
